61cdd3c86415fc66d6cbae5049ce49aa5c686a22
Petar Nenov <petarnenovpetrov@gmail.com>
2022-10-01 09:28:08 +0300

    fix: button style

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const BaseButton = styled.button`
-.button - container {
+.button - container {  
   min-width: 165px;
   width: auto;
   height: 50px;
