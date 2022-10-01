4d9e1f29a6855cdf14c6747410403dacb3e2ab82
Petar Nenov <petarnenovpetrov@gmail.com>
2022-10-01 15:55:00 +0300

    closes #3 fix eslint deps

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,8 @@ import { render, screen } from './utils/test/test-utils';
 import App from './App';
 
 describe('App.js', () => {
-  beforeEach(() => {
-    render(<App />);
-  });
   test('should render [Logo] icon', () => {
+    render(<App />);
     const logoLink = screen.getByRole('link', {
       name: 'crown.svg',
     });
@@ -15,9 +13,10 @@ describe('App.js', () => {
     expect(logoLink).toHaveAttribute('href', '/');
     expect(logoLink).toHaveClass('logo-container');
 
-    //screen.debug(logo);
+    //screen.debug(logoLink);
   });
   test('should render [SIGN IN] text', () => {
+    render(<App />);
     const signInLink = screen.getByRole('link', {
       name: /SIGN IN/i,
     });
