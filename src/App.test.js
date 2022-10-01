d3998758a98df6dfc185e6a110df901b7895f79e
Petar Nenov <petarnenovpetrov@gmail.com>
2022-10-01 15:19:13 +0300

    closes #2 tests updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from './utils/test/test-utils';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App.js', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  test('should render [Logo] icon', () => {
+    const logoLink = screen.getByRole('link', {
+      name: 'crown.svg',
+    });
+
+    expect(logoLink).toBeInTheDocument();
+    expect(logoLink).toHaveAttribute('href', '/');
+    expect(logoLink).toHaveClass('logo-container');
+
+    //screen.debug(logo);
+  });
+  test('should render [SIGN IN] text', () => {
+    const signInLink = screen.getByRole('link', {
+      name: /SIGN IN/i,
+    });
+
+    expect(signInLink).toBeInTheDocument();
+    expect(signInLink).toHaveAttribute('href', '/auth');
+    expect(signInLink).toHaveClass('nav-link');
+
+    //screen.debug(signInLink);
+  });
 });
